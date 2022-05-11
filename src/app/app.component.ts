4c3de1546a450432048dbe5f86c8f6279771bf42
juusovas <juuso.vastila@gmail.com>
2022-05-11 18:07:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Shopping } from "./shopping";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'application';
+  title = 'Project x';
+
+  shoppingValue!: string;
+  list: Shopping[] | undefined;
+
+  ngOnInit() {
+    this.list = [];
+    this.shoppingValue = "";
+  }
+
+  // Tuotteen lisäys & tarkistetaan, että ei ole tyhjä string
+  addItem() {
+    if (this.shoppingValue !== "") {
+      const newItem: Shopping = {
+        id: Date.now(),
+        value: this.shoppingValue,
+        isDone: false
+      };
+      this.list?.push(newItem);
+    }
+    this.shoppingValue = "";
+  }
+
+  // Tuotteen poisto. Annetaan poistettavan tuotteen id parametrina.
+  deleteItem(id: number) {
+    this.list = this.list?.filter(item => item.id !== id);
+  }
+
+
 }
---
+++ src/app/shopping.ts
@@ -0,0 +1,5 @@
+export interface Shopping {
+    id: number;
+    value: string;
+    isDone: boolean;
+}
